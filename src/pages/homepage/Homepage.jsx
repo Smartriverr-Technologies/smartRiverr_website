51c26dd10ebfe78f869ce6ac494df42274874c10
Smartriverr-Technologies <info@smartriverr.com>
2025-05-21 18:34:04 +0530

    testing

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,16 +1,16 @@
 
 import Hero from '../../components/hero/Hero';
-import OurProcess from '../../components/ourprocess/Ourprocess';
+// import OurProcess from '../../components/ourprocess/Ourprocess';
 
-// import Whatweoffer from '../../components/whatweoffer/Whatweoffer';
+import Whatweoffer from '../../components/whatweoffer/Whatweoffer';
 
 const Homepage = () => {
   return (
     <>
     {/* <Navbar/> */}
     <Hero/>
-    <OurProcess/>
-    {/* <Whatweoffer/> */}
+    {/* <OurProcess/> */}
+    <Whatweoffer/>
 
     
     </>
