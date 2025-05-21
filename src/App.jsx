b2c1b80197d75671132639207e9f1dc665ef86d8
Smartriverr-Technologies <info@smartriverr.com>
2025-05-21 18:22:26 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import Navbar from './components/navbar/Navbar.jsx';
+// import Navbar from './components/navbar/Navbar.jsx';
 import Homepage from './pages/homepage/Homepage.jsx';
 
 
 function App() {
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Homepage/> 
      
 
