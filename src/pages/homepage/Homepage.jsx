55c125eb86d6602316e3cd38b93f407e97fc35b6
Smartriverr-Technologies <info@smartriverr.com>
2025-05-22 16:44:44 +0530

    styling

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 
 import Hero from '../../components/hero/Hero';
 import OurProcess from '../../components/ourprocess/OurProcess';
-
+import Navbar from '../../components/navbar/Navbar';
 import Whatweoffer from '../../components/whatweoffer/Whatweoffer';
 import Whychooseus from '../../components/whychooseus/Whychooseus';
 
